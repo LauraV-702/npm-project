00fe3eb3340dbb701b150d82bd19a488185b3437
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-01-06 18:30:43 -0800

    Added and updated package dependencies

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 import lodash from 'lodash';
 import chalk from 'chalk';
+import { faker } from '@faker-js/faker';
+import validator from 'validator';
+// import { colors } from 'colors';
 
 const array = [1,2,3,4,1,3,4,4,5,1,1,4,];
 const uniqueNums = lodash.uniq(array);
 console.log(uniqueNums);
+console.log(chalk.blueBright("HELLO"));
+console.log(chalk.yellow("Hello, with Chalk!"));
 
-console.log(chalk.bgGray("HELLO"));
-console.log(chalk.blueBright("Hello, with Chalk!"));
+
+// let fakeAccount = {
+//     userId: faker.string.uuid(),
+//     username: faker.internet.username(),
+//     email: faker.internet.email(),
+//     registeredAt: faker.date.past()
+// };
+
+// console.log(fakeAccount.userId);
+// console.log(fakeAccount.username);
+// console.log(fakeAccount.email);
+// console.log(fakeAccount.registeredAt);
